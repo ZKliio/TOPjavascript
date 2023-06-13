ba7a8abf2a5b76cf6eefc2ad208bef713b634fb6
ZKliio <limzukai@gmail.com>
2023-06-13 15:25:17 +0800

    rockpaperscissors browser enabled.
    rock paper scissors.
    
    object logic

---
+++ rockpaperscissors/rockpaperscissors.js
@@ -0,0 +1,54 @@
+choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice(){
+  int_gen = Math.floor(Math.random()*3);
+  let computer_choice = choices[int_gen];
+  return computer_choice
+}
+
+function startGame(){
+  // prompt for a choice by user
+  let playerSelection = (prompt("rock, paper, scissors?")).toLowerCase();
+  if (choices.indexOf(playerSelection) == -1){
+    alert("Invalid Input!")
+    
+    return;
+  }
+  let computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection);
+  
+  
+  return;
+}
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  
+  //logic behind win lose
+  console.log(playerSelection, computerSelection)
+  const outcomes = {
+    rock: {loseTo: "paper", winTo: "scissors"},
+    paper: {loseTo: "scissors", winTo: "rock"},
+    scissors: {loseTo: "rock", winTo: "paper"}
+  }
+
+
+  if (playerSelection == computerSelection){
+    alert("You Drew")
+    console.log("Draw")
+  }
+  else{
+    if (outcomes[playerSelection].loseTo === computerSelection){
+      alert("You Lost")
+      console.log("Lose")
+    }
+    else if (outcomes[playerSelection].winTo === computerSelection)
+      alert("You Won")
+      console.log("Win")
+  }
+}
+
+
+// const playerSelection = "Rock".toLowerCase();
+
+// console.log(playRound(playerSelection, computerSelection));
